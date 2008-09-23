e7bed845f5850b3cdb232e0bcfb9314904ea1cca
Ben VandenBos <ben@avvo.com>
2008-09-23 15:18:33 -0700

    updating readme

--- lib/render_partial_comments.rb
+++ lib/render_partial_comments.rb
@@ -1,27 +1,27 @@
 # these should all be defined...
 ActionView::Base
   
-module ActionView::Base
-   
+class ActionView::Base
+  
   private
-
+  
   def self.perform_partial_comments?
     # TODO: make this some kind of configuration in environments
     RAILS_ENV == 'development'
   end
   
-  # don't even define this method if we don't want to 
-  if preform_partial_comments?
+  # don't even define this method if we don't want it enabled
+  if perform_partial_comments?
 
     # render_partial(partial_path, object_assigns = nil, local_assigns = {})
     def render_partial_with_comments(partial_path, object_assigns = nil, local_assigns = {})
-      r = ''    
-      r << %{<!-- render_start '#{template_path}' -->} if self.class.perform_partial_comments?    
-      r << render_for_file_without_comments(template_path, state, layout, locals)
-      r << %{<!-- render_end '#{template_path}' -->} if self.class.perform_partial_comments?
+      r = %{<!-- render_begin '#{partial_path}' -->}
+      r << render_partial_without_comments(partial_path, object_assigns, local_assigns)
+      r << %{<!-- render_end '#{partial_path}' -->}
       r
     end
 
     alias_method_chain :render_partial, :comments
+    
   end
 end
